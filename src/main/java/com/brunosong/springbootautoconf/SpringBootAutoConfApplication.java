25ad6b1b7e9d94e0a347bbe1950047b2f3fe9984
BrunoSong <brunosong@wjtb.net>
2024-02-19 08:57:11 +0900

    @SpringBootApplication 없이 embedded tomcat 사용으로 톰캣 띄우기

--- src/main/java/com/brunosong/springbootautoconf/SpringBootAutoConfApplication.java
+++ src/main/java/com/brunosong/springbootautoconf/SpringBootAutoConfApplication.java
@@ -1,13 +1,42 @@
 package com.brunosong.springbootautoconf;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
 
-@SpringBootApplication
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
 public class SpringBootAutoConfApplication {
 
+    /* servletContext.addServlet : 서블릿을 등록 해준다. web.xml의 <servlet-mapping><servlet-name>hello</..></..>  */
+    /* servletContext.addServlet.addMapping : 서블릿의 호출 경로를 맵핑해준다. web.xml의 <servlet-mapping><url-pattern>/hello</..></..>  */
+
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootAutoConfApplication.class, args);
+
+        TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+
+            /* hello 서블릿을 만들어 /hello 요청으로 맵핑을 한다. */
+            servletContext.addServlet("hello", new HttpServlet() {
+                        @Override
+                        protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                            //웹 응답(response)의 3가지 요소를 갖춘다. (상태코드,헤더,바디)
+                            resp.setStatus(200);
+                            resp.setHeader("Content-Type", "text/plain");
+                            PrintWriter writer = resp.getWriter();
+                            writer.println("Hello My name is brunosong");
+                        }
+                    }
+            ).addMapping("/hello");
+        });
+
+        webServer.start();
+
     }
 
 }
