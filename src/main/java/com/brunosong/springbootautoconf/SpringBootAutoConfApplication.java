ea98d6d406bacc96d1f2d13ee1122553a993c81d
BrunoSong <brunosong@wjtb.net>
2024-02-19 08:21:59 +0900

    git commit -m "스프링부트 3.2 시작"

---
+++ src/main/java/com/brunosong/springbootautoconf/SpringBootAutoConfApplication.java
@@ -0,0 +1,13 @@
+package com.brunosong.springbootautoconf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAutoConfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAutoConfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/brunosong/springbootautoconf/SpringBootAutoConfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunosong.springbootautoconf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAutoConfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
