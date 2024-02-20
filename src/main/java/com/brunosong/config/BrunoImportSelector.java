3937598aedb95ba0a0ebe913d51a1da947c592e0
brunosong <playprogramer@gmail.com>
2024-02-20 23:07:45 +0900

    ImportSelector 적용 2단계 ( BrunoAutoConfiguration ) 적용

--- src/main/java/com/brunosong/config/BrunoImportSelector.java
+++ src/main/java/com/brunosong/config/BrunoImportSelector.java
@@ -1,16 +1,27 @@
 package com.brunosong.config;
 
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.ImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BrunoImportSelector implements ImportSelector {
+
+    private final ClassLoader classloader;
+
+    public BrunoImportSelector(ClassLoader classloader) {
+        this.classloader = classloader;
+    }
 
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        return new String[] {
-                "com.brunosong.config.autoconfig.DispatcherServletConfig",
-                "com.brunosong.config.autoconfig.ServletWebServerConfig"
-        };
+
+        List<String> list = new ArrayList<>();
+        ImportCandidates.load(BrunoAutoConfiguration.class, classloader).forEach(list::add);
+
+        return list.toArray(new String[0]);
     }
 
 }
