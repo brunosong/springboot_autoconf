12ebfed269d029ba3bf17a81bb248311540d919e
BrunoSong <brunosong@wjtb.net>
2024-02-20 11:00:31 +0900

    스프링부트 기본 구조와 비슷하게 구성

--- src/main/java/com/brunosong/springbootautoconf/SpringBootAutoConfApplication.java
+++ src/main/java/com/brunosong/springbootautoconf/SpringBootAutoConfApplication.java
@@ -1,12 +1,10 @@
 package com.brunosong.springbootautoconf;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 
@@ -24,35 +22,10 @@ public class SpringBootAutoConfApplication {
         return new DispatcherServlet();
     }
 
-
     public static void main(String[] args) {
-
-        /* 스프링에서 가장 많이 쓰는 Annotation으로 설정을 사용하겠다. */
-        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-            @Override
-            protected void onRefresh() {
-                super.onRefresh();
-
-                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
-                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-                /*
-                * 이 부분이 없어도 되는 이유는 ApplicationContextAware setApplicationContext(ApplicationContext applicationContext) 을
-                * DispatcherServlet이 상속을 받고 있기 때문에 스프링이 알아서 자동으로 주입시켜준다.
-                * */
-                //dispatcherServlet.setApplicationContext(this);
-
-                WebServer webServer = serverFactory.getWebServer(servletContext -> {
-                    servletContext.addServlet("dispatcherServlet", dispatcherServlet )
-                            .addMapping("/*");
-                });
-
-                webServer.start();
-            }
-        };
-
-        applicationContext.register(SpringBootAutoConfApplication.class);
-        applicationContext.refresh();
-
+        MySpringBootApplication.run(SpringBootAutoConfApplication.class, args);
     }
+
+
 
 }
