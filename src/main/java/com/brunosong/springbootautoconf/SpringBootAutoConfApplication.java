7984817397387ffe4d4f2c84d1030eff87d05bc3
BrunoSong <brunosong@wjtb.net>
2024-02-20 10:10:11 +0900

    스프링컨테이너안에 디스패쳐서블릿 탑재후 스프링으로 통합

--- src/main/java/com/brunosong/springbootautoconf/SpringBootAutoConfApplication.java
+++ src/main/java/com/brunosong/springbootautoconf/SpringBootAutoConfApplication.java
@@ -13,19 +13,29 @@ public class SpringBootAutoConfApplication {
 
     public static void main(String[] args) {
 
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+        /*
+        * 이전 소스에서는 servlet 부분과 스프링컨테이너 부분이 따로 나뉘어 있었다
+        * 이젠 스프링컨테이너안에 디스패쳐를 넣어줘서 스프링으로 통합해준다.
+        *  */
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
+
+                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                WebServer webServer = serverFactory.getWebServer(servletContext -> {
+                    servletContext.addServlet("dispatcherServlet", new DispatcherServlet( this ) )
+                            .addMapping("/*");
+                });
+
+                webServer.start();
+            }
+        };
+
         applicationContext.registerBean( MemberController.class );
         applicationContext.registerBean( OrderController.class );
         applicationContext.refresh();
 
-        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-
-        WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("dispatcherServlet", new DispatcherServlet( applicationContext ) )
-                    .addMapping("/*");
-        });
-
-        webServer.start();
 
     }
 
