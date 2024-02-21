f84343590bddd28254ea1024c36f8e690d492c3a
brunosong <playprogramer@gmail.com>
2024-02-21 09:47:09 +0900

    Condition을 사용하여 서버의 클래스 존재 여부로 판단

--- src/main/java/com/brunosong/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/com/brunosong/config/autoconfig/TomcatWebServerConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.core.type.AnnotatedTypeMetadata;
+import org.springframework.util.ClassUtils;
 
 @BrunoAutoConfiguration
 @Conditional(TomcatWebServerConfig.TomcatWebServerCondition.class)
@@ -21,7 +22,8 @@ public class TomcatWebServerConfig {
 
         @Override
         public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            return false;
+            return ClassUtils.isPresent("org.apache.catalina.startup.Tomcat",
+                    context.getClassLoader());
         }
     }
 
