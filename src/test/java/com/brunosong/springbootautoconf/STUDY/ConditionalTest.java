cf30a9067838db518afc7fe12e5e2191ed463bfa
brunosong <playprogramer@gmail.com>
2024-02-21 09:12:39 +0900

    Condition 적용 테스트 2 ( ApplicationContextRunner ) 사용

--- src/test/java/com/brunosong/springbootautoconf/STUDY/ConditionalTest.java
+++ src/test/java/com/brunosong/springbootautoconf/STUDY/ConditionalTest.java
@@ -2,6 +2,9 @@ package com.brunosong.springbootautoconf.STUDY;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.*;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 
@@ -11,21 +14,25 @@ public class ConditionalTest {
     @Test
     void condition(){
 
-        AnnotationConfigApplicationContext ac1 = new AnnotationConfigApplicationContext();
-        ac1.register(Config1.class);
-        ac1.refresh();
+        // 스프링 부트에서 제공하는 ApplicationContext 전용 테스트 객체
+        // 빈을 가지고 있는지 테스트
+        ApplicationContextRunner ar1 = new ApplicationContextRunner();
+        ar1.withUserConfiguration(Config1.class)
+                .run( context -> {
+                    Assertions.assertThat(context).hasSingleBean(StudyBean.class);
+                    Assertions.assertThat(context).hasSingleBean(Config1.class);
+                });
 
-        StudyBean studyBean1 = ac1.getBean("studyBean", StudyBean.class);
-        Assertions.assertThat(studyBean1).isNotNull();
 
+        // 빈을 가지고 있지 않는지 테스트
+        ApplicationContextRunner ar2 = new ApplicationContextRunner();
+        ar2.withUserConfiguration(Config2.class)
+                .run( context -> {
+                    Assertions.assertThat(context).doesNotHaveBean(Config2.class);
+                    Assertions.assertThat(context).doesNotHaveBean(StudyBean.class);
 
-        AnnotationConfigApplicationContext ac2 = new AnnotationConfigApplicationContext();
-        ac2.register(Config2.class);
-        ac2.refresh();
+                });
 
-        Assertions.assertThatThrownBy(() -> {
-            ac2.getBean("studyBean", StudyBean.class);
-        });
 
     }
 
