44bce5efe644c601b343a19b7475889618286b4b
BrunoSong <brunosong@wjtb.net>
2024-02-22 09:39:16 +0900

    ServerProperties를 빈으로 등록하여 서버에 프로퍼티 정보를 주입

--- src/main/java/com/brunosong/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/com/brunosong/config/autoconfig/TomcatWebServerConfig.java
@@ -2,27 +2,37 @@ package com.brunosong.config.autoconfig;
 
 import com.brunosong.config.BrunoAutoConfiguration;
 import com.brunosong.config.BrunoConditionalOnClass;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @BrunoAutoConfiguration
 @BrunoConditionalOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
 
-    @Value("${contextPath}")
-    private String contextPath;
-
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean
-    public ServletWebServerFactory servletWebServerFactory() {
+    public ServletWebServerFactory servletWebServerFactory( ServerProperties serverProperties ) {
 
         TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        serverFactory.setContextPath(contextPath);
+        serverFactory.setContextPath(serverProperties.getContextPath());
+        serverFactory.setPort(serverProperties.getPort());
 
         return serverFactory;
+    }
+
+
+    @Bean
+    ServerProperties serverProperties(Environment environment) {
+
+        ServerProperties serverProperties = new ServerProperties();
+
+        serverProperties.setPort(Integer.parseInt(environment.getProperty("port")));
+        serverProperties.setContextPath(environment.getProperty("contextPath"));
+
+        return serverProperties;
     }
 
 }
