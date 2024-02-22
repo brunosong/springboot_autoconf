af4de051be2bd6e465c84f36267206a8501f7d45
BrunoSong <brunosong@wjtb.net>
2024-02-22 09:47:10 +0900

    Binder를 사용하여 ServerProperties에 프로퍼티들을 자동으로 맵핑시켜준다.

--- src/main/java/com/brunosong/config/autoconfig/ServerPropertiesConfig.java
+++ src/main/java/com/brunosong/config/autoconfig/ServerPropertiesConfig.java
@@ -1,6 +1,7 @@
 package com.brunosong.config.autoconfig;
 
 import com.brunosong.config.BrunoAutoConfiguration;
+import org.springframework.boot.context.properties.bind.Binder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 
@@ -10,12 +11,8 @@ public class ServerPropertiesConfig {
     @Bean
     ServerProperties serverProperties(Environment environment) {
 
-        ServerProperties serverProperties = new ServerProperties();
-
-        serverProperties.setPort(Integer.parseInt(environment.getProperty("port")));
-        serverProperties.setContextPath(environment.getProperty("contextPath"));
-
-        return serverProperties;
+        /* 일일이 하나하나 맵핑을 하려면 너무 힘들기 때문에 Binder를 사용하여 자동으로 맵핑을 하게 해준다. */
+        return Binder.get(environment).bindOrCreate("", ServerProperties.class);
     }
 
 }
